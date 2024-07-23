27fa4e7d091aa29d479f0ad07826614936bdfd8a
unknown <farrel.hunter85@gmail.com>
2024-07-23 21:36:51 +0700

    Create Project

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC67bn6f71jMQ5rb1f7hbW96ibvW6D67_w",
+  authDomain: "netflix-clone-1489d.firebaseapp.com",
+  projectId: "netflix-clone-1489d",
+  storageBucket: "netflix-clone-1489d.appspot.com",
+  messagingSenderId: "1028452173877",
+  appId: "1:1028452173877:web:d1a2c61e106d402601097c",
+};
