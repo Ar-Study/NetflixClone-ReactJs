8d6185feb27d40437556bb030d694ac663b24906
unknown <farrel.hunter85@gmail.com>
2024-08-17 21:31:09 +0700

    enyelesaikan Halaman Utaman

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -2,12 +2,29 @@ import React from "react";
 import Banner from "./Banner";
 import "./HomeScreen.css";
 import Nav from "./Nav";
+import requests from "./Requests";
+import Row from "./Row";
 
 function HomeScreen() {
   return (
     <div className="homescreen">
       <Banner />
       <Nav />
+      <Row
+        title="Netflix original"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovie} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovie} />
+      <Row
+        title="Documentaries Movies"
+        fetchUrl={requests.fetchDocumentaries}
+      />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovie} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovie} />
       {/* Banner */}
       {/* Row */}
     </div>
