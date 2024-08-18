1edc1abbcf090030debb8edb3f260534c70dad23
unknown <farrel.hunter85@gmail.com>
2024-08-18 19:54:55 +0700

    Sedikit tambahan pada homepage untuk memastikan gambar muncul

--- src/Row.js
+++ src/Row.js
@@ -5,6 +5,7 @@ import "./Row.css";
 function Row({ title, fetchUrl, isLargeRow = false }) {
   const [movies, setMovies] = useState([]);
   const base_url = "https://image.tmdb.org/t/p/original/";
+
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
@@ -15,22 +16,30 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
     fetchData();
   }, [fetchUrl]);
 
-  console.log(movies);
   return (
     <div className="row">
       <h2>{title}</h2>
 
-      <div class="row__posters">
-        {movies.map((movie) => (
-          <img
-            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-            key={movie.id}
-            src={`${base_url}${
-              isLargeRow ? movie.poster_path : movie.backdrop_path
-            }`}
-            alt={movie}
-          />
-        ))}
+      <div className="row__posters">
+        {movies.map((movie) => {
+          // Only render the image if the required path is available
+          if (
+            (isLargeRow && movie.poster_path) ||
+            (!isLargeRow && movie.backdrop_path)
+          ) {
+            return (
+              <img
+                className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                key={movie.id}
+                src={`${base_url}${
+                  isLargeRow ? movie.poster_path : movie.backdrop_path
+                }`}
+                alt={movie.name || movie.title || movie.original_name}
+              />
+            );
+          }
+          return null; // Return null if no image path is available
+        })}
       </div>
     </div>
   );
